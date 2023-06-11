f490473af5e258d3d620931d31fc7b48191ca520
khsaad04 <khsaad0404@gmail.com>
2023-06-11 12:06:09 +0600

    init

--- .config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q polybar
-
-# Launch bar
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
