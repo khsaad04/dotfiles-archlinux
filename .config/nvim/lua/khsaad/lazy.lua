855649d2b52fb79ccb14f6999e29f8bdfbc16738
khsaad04 <khsaad0404@gmail.com>
2023-07-26 15:31:29 +0600

    Wed Jul 26 03:31:29 PM +06 2023

--- .config/nvim/lua/khsaad/lazy.lua
+++ .config/nvim/lua/khsaad/lazy.lua
@@ -25,9 +25,6 @@ require('lazy').setup {
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
