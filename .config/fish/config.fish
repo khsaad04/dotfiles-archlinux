05c89b96d53bd4f25cda7271b4a2f3fd4e155864
khsaad04 <khsaad0404@gmail.com>
2023-08-16 22:08:54 +0600

    Wed Aug 16 10:08:54 PM +06 2023

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+set -g fish_greeting
+fish_add_path -aP ~/.cargo/bin
+fish_add_path -aP ~/.local/bin
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias vim='nvim'
+alias ls='exa'
+alias la='exa -la'
+
+if status is-interactive
+    starship init fish | source
+end
---
+++ .config/nvim/lua/khsaad/options.lua
@@ -0,0 +1,45 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+
+-- Set highlight on search
+vim.o.hlsearch = true
+
+-- Make line numbers default
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
