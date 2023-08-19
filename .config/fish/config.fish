c3676526066d012b5c1b642107ec7f98d9a4d84f
khsaad04 <khsaad0404@gmail.com>
2023-08-19 14:48:37 +0600

    Sat Aug 19 02:48:37 PM +06 2023

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 set -g fish_greeting
+
+set -gx MOZ_ENABLE_WAYLAND 1
+
 fish_add_path -aP ~/.cargo/bin
 fish_add_path -aP ~/.local/bin
+
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias vim='nvim'
 alias ls='exa'
