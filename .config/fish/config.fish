f3bdd9a7985c773a55a66cc5bc6ae35c9ca72730
KH Saad <88236024+khsaad04@users.noreply.github.com>
2023-09-10 18:03:50 +0600

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,6 @@
+set -g fish_greeting
+fish_add_path -aP ~/.local/bin
+
 if status is-interactive
     starship init fish | source
 end
-
-set -g fish_greeting
-fish_add_path -aP ~/.local/bin
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias vim='nvim'
-alias ls='exa'
-alias la='exa -la'
