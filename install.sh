84ed00d808ab12d272f530ac82b97a9238115c7a
KH Saad <88236024+khsaad04@users.noreply.github.com>
2023-12-31 15:52:39 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ makepkg -si
 cd
 
 # Installing necessary packages
-paru -Syu hyprland swww waybar-hyprland pipewire pipewire-pulse pipewire-jack pamixer wireplumber xorg-xwayland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland grim slurp wofi kitty fish starship cava-git neofetch btop bat neovim ripgrep fd unzip npm wl-clipboard dunst libnotify sddm ttf-firacode-nerd noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk catppuccin-gtk-theme-mocha papirus-icon-theme polkit-gnome mesa vulkan-intel firefox --needed --noconfirm
+paru -Syu hyprland swww waybar-hyprland pipewire pipewire-pulse pipewire-jack pamixer wireplumber xorg-xwayland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland grim slurp wofi kitty fish starship cava-git neofetch btop bat neovim ripgrep fd unzip npm wl-clipboard dunst libnotify sddm ttf-firacode-nerd noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk catppuccin-gtk-theme-mocha papirus-icon-theme polkit-gnome mesa vulkan-intel firefox --needed
 
 # Copying over dotfiles
 git clone --depth 1 -b hyprland --single-branch https://github.com/khsaad04/dotfiles.git
