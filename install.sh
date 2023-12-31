8a140c145f43c9553fde250efff4a527e571e6a9
KH Saad <88236024+khsaad04@users.noreply.github.com>
2023-12-31 17:18:48 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,16 @@
 paru -Syu hyprland swww waybar-hyprland pipewire pipewire-pulse pipewire-jack pamixer wireplumber xorg-xwayland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland grim slurp wofi kitty fish starship cava-git neofetch btop bat neovim ripgrep fd unzip npm wl-clipboard dunst libnotify sddm ttf-firacode-nerd noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk catppuccin-gtk-theme-mocha papirus-icon-theme polkit-gnome mesa vulkan-intel firefox --needed --noconfirm
 
 # Copying over dotfiles
+if [ -d "dotfiles" ]; then
+  rm -rf dotfiles
+fi
 git clone --depth 1 -b hyprland --single-branch https://github.com/khsaad04/dotfiles.git
+
+if [ ! -d ".config" ]; then
+  mkdir .config
+fi
 cp -r dotfiles/.config/* ~/.config
 cp -r dotfiles/.local/* ~/.local
 
 # Switching default shell to fish
-chsh -s /bin/fish
+sudo chsh -s /bin/fish
