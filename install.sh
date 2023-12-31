cebdf83ffcab4eff17512884d295935f2c419cab
KH Saad <88236024+khsaad04@users.noreply.github.com>
2023-12-31 15:57:32 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Installing paru
-sudo pacman -S rust --noconfirm
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-cd
-
 # Installing necessary packages
 paru -Syu hyprland swww waybar-hyprland pipewire pipewire-pulse pipewire-jack pamixer wireplumber xorg-xwayland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland grim slurp wofi kitty fish starship cava-git neofetch btop bat neovim ripgrep fd unzip npm wl-clipboard dunst libnotify sddm ttf-firacode-nerd noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk catppuccin-gtk-theme-mocha papirus-icon-theme polkit-gnome mesa vulkan-intel firefox --needed
 
