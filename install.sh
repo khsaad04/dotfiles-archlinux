cd952ee87302087515486585202334991d731858
KH Saad <88236024+khsaad04@users.noreply.github.com>
2024-01-08 13:07:17 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Installing necessary packages
-paru -Syu hyprland swww waybar-hyprland pipewire pipewire-pulse pipewire-jack pamixer wireplumber xorg-xwayland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland grim slurp wofi kitty fish starship cava-git neofetch btop bat neovim ripgrep fd unzip npm wl-clipboard dunst libnotify sddm ttf-firacode-nerd noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk catppuccin-gtk-theme-mocha papirus-icon-theme polkit-gnome firefox --needed --noconfirm
+paru -Syu hyprland swww waybar-hyprland pipewire pipewire-pulse pipewire-jack pamixer wireplumber xorg-xwayland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland grim slurp wofi kitty fish starship cava-git neofetch btop bat neovim ripgrep fd unzip npm wl-clipboard dunst libnotify ttf-firacode-nerd catppuccin-gtk-theme-mocha papirus-icon-theme polkit-gnome --needed --noconfirm
 
 # Copying over dotfiles
 git clone --depth 1 -b hyprland --single-branch https://github.com/khsaad04/dotfiles.git khsaad-dotfiles
