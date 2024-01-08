54dacd3adbcd99adee5cee27f006059987c4a79a
KH Saad <88236024+khsaad04@users.noreply.github.com>
2024-01-08 15:38:44 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Installing necessary packages
-paru -Syu hyprland swww waybar-hyprland pamixer grim slurp wofi kitty fish starship cava-git neofetch wl-clipboard dunst libnotify ttf-firacode-nerd catppuccin-gtk-theme-mocha papirus-icon-theme --needed --noconfirm
+# Installing packages
+echo "Starting installing packages"
+sleep 1
+paru -Syu hyprland swww waybar-hyprland pamixer wofi kitty fish starship dunst libnotify ttf-firacode-nerd catppuccin-gtk-theme-mocha papirus-icon-theme --needed --noconfirm && echo "installing packages complete"
+sleep 1
 
-# Copying over dotfiles
+# Copying dotfiles
+echo "cloning and copying over dotfiles"
+sleep 1
 git clone --depth 1 -b hyprland --single-branch https://github.com/khsaad04/dotfiles.git /tmp/khsaad-dotfiles
 cp -r /tmp/khsaad-dotfiles/.config/* ~/.config
 
 # Switching default shell to fish
+echo "Changing shell"
+sleep 1
 sudo chsh -s /bin/fish
