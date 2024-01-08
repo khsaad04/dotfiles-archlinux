40de29c59c92d8355b327e3edc19fa38cf492cd9
KH Saad <88236024+khsaad04@users.noreply.github.com>
2024-01-08 12:48:13 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 # Installing necessary packages
-paru -Syu hyprland swww waybar-hyprland pipewire pipewire-pulse pipewire-jack pamixer wireplumber xorg-xwayland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland grim slurp wofi kitty fish starship cava-git neofetch btop bat neovim ripgrep fd unzip npm wl-clipboard dunst libnotify sddm ttf-firacode-nerd noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk catppuccin-gtk-theme-mocha papirus-icon-theme polkit-gnome mesa vulkan-intel firefox --needed --noconfirm
+paru -Syu hyprland swww waybar-hyprland pipewire pipewire-pulse pipewire-jack pamixer wireplumber xorg-xwayland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland grim slurp wofi kitty fish starship cava-git neofetch btop bat neovim ripgrep fd unzip npm wl-clipboard dunst libnotify sddm ttf-firacode-nerd noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk catppuccin-gtk-theme-mocha papirus-icon-theme polkit-gnome firefox --needed --noconfirm
 
 # Copying over dotfiles
-if [ -d "dotfiles" ]; then
-  rm -rf dotfiles
-fi
-git clone --depth 1 -b hyprland --single-branch https://github.com/khsaad04/dotfiles.git
+git clone --depth 1 -b hyprland --single-branch https://github.com/khsaad04/dotfiles.git khsaad-dotfiles
 
 if [ ! -d ".config" ]; then
   mkdir .config
 fi
-cp -r dotfiles/.config/* ~/.config
-cp -r dotfiles/.local/* ~/.local
+
+cp -r khsaad-dotfiles/.config/* ~/.config
+cp -r khsaad-dotfiles/.local/* ~/.local
 
 # Switching default shell to fish
 sudo chsh -s /bin/fish
