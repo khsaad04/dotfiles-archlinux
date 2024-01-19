8d92bcd6df3010542dbb8c4f4224f1d62ec1c4f6
khsaad04 <khsaad0404@gmail.com>
2024-01-19 10:54:38 +0600

    update README

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is for Arch or Arch based linux distros. But you can try this on any distro
 
 - ##### Dependencies
 ```sh
-hyprland swww waybar-hyprland pamixer wofi kitty fish starship dunst libnotify ttf-firacode-nerd catppuccin-gtk-theme-mocha papirus-icon-theme
+hyprland swww waybar-hyprland pamixer wofi foot fish starship dunst libnotify ttf-firacode-nerd catppuccin-gtk-theme-mocha papirus-icon-theme
 ```
 
 - ##### Optional Dependencies
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Installing packages
 echo "Starting installing packages"
 sleep 1
-paru -Syu hyprland swww waybar-hyprland pamixer wofi kitty fish starship dunst libnotify ttf-firacode-nerd catppuccin-gtk-theme-mocha papirus-icon-theme --needed --noconfirm && echo "installing packages complete"
+paru -Syu hyprland swww waybar-hyprland pamixer wofi foot fish starship dunst libnotify ttf-firacode-nerd catppuccin-gtk-theme-mocha papirus-icon-theme --needed --noconfirm && echo "installing packages complete"
 sleep 1
 
 # Copying dotfiles
