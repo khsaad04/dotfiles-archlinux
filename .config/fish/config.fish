f824d3e07e183bb2ec920f3b2e0246e6d0082bf7
khsaad04 <khsaad0404@gmail.com>
2024-08-16 14:51:45 +0600

    refactor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,12 +7,12 @@ status is-interactive; and begin
     alias lla 'eza -la'
     alias ls eza
     alias lt 'eza --tree'
-    alias t 'tmux attach || tmux new-session -s main tms'
     alias vimdiff 'nvim -d'
 
     # Interactive shell initialisation
     set fish_greeting
     fish_vi_key_bindings
+    fish_add_path -aP ~/.config/scripts
 
     if test "$TERM" != dumb
         eval (starship init fish)
