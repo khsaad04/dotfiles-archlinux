a34cc356442612c6703eb0b2dbdbb04ad3276d98
KH Saad <88236024+khsaad04@users.noreply.github.com>
2024-08-19 14:47:43 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,6 @@ sleep 1
 paru -Syu swayfx swaybg hyprland waybar wofi wezterm fish starship dunst libnotify wireplumber ttf-iosevka ttf-nerd-fonts-symbols adw-gtk-theme adwaita-icon-theme grimblast wl-clipboard wl-clip-persist-git --noconfirm --needed && echo "Installing required dependencies complete"
 sleep 1
 
-read -p "Do you want to clean your system[DO NOT DO IT IF THE PREVIOUS INSTALLATION FAILED]? (y/n): " yn
-case $yn in
-    y ) paru -c && paru -Sc --noconfirm;;
-    n ) echo "Aight bet";;
-    * ) echo "Invalid response";
-        exit 1;;
-esac
-sleep 1
-
 # Copying dotfiles
 echo "cloning and copying over dotfiles"
 sleep 1
