f09dae97623642c8d71c2fc069b9781b8c5f6b71
khsaad04 <khsaad0404@gmail.com>
2024-09-11 23:10:07 +0600

    fix install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ dependencies="swayfx hyprland swaybg waybar wofi wezterm fish starship dunst lib
 paru -Syu $dependencies --noconfirm --needed
 sleep 1
 
-rm -rf /tmp/khsaad-dotfiles;git clone --depth 1 https://github.com/khsaad04/dotfiles-archlinux.git /tmp/khsaad-dotfiles
+rm -rf /tmp/khsaad-dotfiles || true
+sleep 1
+git clone --depth 1 https://github.com/khsaad04/dotfiles-archlinux.git /tmp/khsaad-dotfiles
 cp -r /tmp/khsaad-dotfiles/.config/* ~/.config
 sleep 1
 
