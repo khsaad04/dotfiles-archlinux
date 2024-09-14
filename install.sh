27ecfec8d54353b62e34abb8347fe0072677809a
khsaad04 <khsaad0404@gmail.com>
2024-09-14 17:45:23 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-dependencies="swayfx hyprland swaybg waybar wofi kitty fish starship dunst libnotify wireplumber ttf-iosevka ttf-nerd-fonts-symbols adw-gtk-theme adwaita-icon-theme grimblast wl-clipboard"
+dependencies="hyprland swaybg waybar wofi kitty fish starship dunst libnotify wireplumber ttf-iosevka ttf-nerd-fonts-symbols adw-gtk-theme adwaita-icon-theme grimblast wl-clipboard"
 
 paru -Syu $dependencies --noconfirm --needed
 sleep 1
@@ -13,4 +13,4 @@ git clone --depth 1 https://github.com/khsaad04/dotfiles-archlinux.git /tmp/khsa
 cp -r /tmp/khsaad-dotfiles/.config/* ~/.config
 sleep 1
 
-sudo chsh -s /bin/fish
+sudo chsh -s $(which fish)
