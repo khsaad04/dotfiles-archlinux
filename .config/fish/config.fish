8c0bb9a6770a83a688da9718ea7ddc75b82bd4e5
khsaad04 <khsaad0404@gmail.com>
2024-09-16 15:47:12 +0600

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,5 @@
 status is-interactive; and begin
-    # Interactive shell initialisation
     set fish_greeting
-    fish_vi_key_bindings
-    fish_add_path -aP ~/.config/scripts
-
     if test "$TERM" != dumb
         eval (starship init fish)
         enable_transience
