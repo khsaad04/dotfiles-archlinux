bb9b262abbd9ef48426881778ce558bf07132710
khsaad04 <khsaad0404@gmail.com>
2024-10-28 22:17:46 +0600

    feat: Integrate with dotman(my personal dotfile manager)

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,7 +8,7 @@ return {
             require("gruvbox").setup({
                 contrast = "hard",
                 palette_overrides = {
-                    dark0_hard = "#13140d",
+                    dark0_hard = "#13140c",
                 },
             })
 
@@ -16,39 +16,39 @@ return {
             custom_gruvbox = {
                 normal = {
                     a = {
-                        bg = "#c2cd7c",
-                        fg = "#2d3400",
+                        bg = "#c5cd7a",
+                        fg = "#2f3300",
                         gui = "bold",
                     },
                     b = {
-                        bg = "#2a2b22",
-                        fg = "#e4e3d6",
+                        bg = "#2a2a22",
+                        fg = "#e5e3d6",
                     },
-                    c = { bg = "#1f2018", fg = "#e4e3d6" },
+                    c = { bg = "#202018", fg = "#e5e3d6" },
                 },
                 insert = {
                     a = {
-                        bg = "#a2d0c2",
-                        fg = "#06372e",
+                        bg = "#a2d0c1",
+                        fg = "#07372d",
                         gui = "bold",
                     },
                     b = {
-                        bg = "#2a2b22",
-                        fg = "#e4e3d6",
+                        bg = "#2a2a22",
+                        fg = "#e5e3d6",
                     },
-                    c = { bg = "#1f2018", fg = "#e4e3d6" },
+                    c = { bg = "#202018", fg = "#e5e3d6" },
                 },
                 visual = {
                     a = {
-                        bg = "#c6c9a7",
-                        fg = "#2f321a",
+                        bg = "#c7c9a6",
+                        fg = "#30321a",
                         gui = "bold",
                     },
                     b = {
-                        bg = "#2a2b22",
-                        fg = "#e4e3d6",
+                        bg = "#2a2a22",
+                        fg = "#e5e3d6",
                     },
-                    c = { bg = "#1f2018", fg = "#e4e3d6" },
+                    c = { bg = "#202018", fg = "#e5e3d6" },
                 },
                 replace = {
                     a = {
@@ -57,38 +57,38 @@ return {
                         gui = "bold",
                     },
                     b = {
-                        bg = "#2a2b22",
-                        fg = "#e4e3d6",
+                        bg = "#2a2a22",
+                        fg = "#e5e3d6",
                     },
-                    c = { bg = "#1f2018", fg = "#e4e3d6" },
+                    c = { bg = "#202018", fg = "#e5e3d6" },
                 },
                 command = {
                     a = {
-                        bg = "#c2cd7c",
-                        fg = "#2d3400",
+                        bg = "#c5cd7a",
+                        fg = "#2f3300",
                         gui = "bold",
                     },
                     b = {
-                        bg = "#2a2b22",
-                        fg = "#e4e3d6",
+                        bg = "#2a2a22",
+                        fg = "#e5e3d6",
                     },
-                    c = { bg = "#1f2018", fg = "#e4e3d6" },
+                    c = { bg = "#202018", fg = "#e5e3d6" },
                 },
                 inactive = {
                     a = {
-                        bg = "#2a2b22",
-                        fg = "#e4e3d6",
+                        bg = "#2a2a22",
+                        fg = "#e5e3d6",
                         gui = "bold",
                     },
                     b = {
-                        bg = "#2a2b22",
-                        fg = "#e4e3d6",
+                        bg = "#2a2a22",
+                        fg = "#e5e3d6",
                     },
-                    c = { bg = "#1f2018", fg = "#e4e3d6" },
+                    c = { bg = "#202018", fg = "#e5e3d6" },
                 },
             }
-            custom_gruvbox.normal.c.bg = "#1f2018"
-            custom_gruvbox.normal.c.fg = "#e4e3d6"
+            custom_gruvbox.normal.c.bg = "#202018"
+            custom_gruvbox.normal.c.fg = "#e5e3d6"
 
             require("lualine").setup({
                 options = { theme = custom_gruvbox },
